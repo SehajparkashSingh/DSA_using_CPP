4906d01428ba4a666b42182fb5c122f1802d2597
SehajparkashSingh <singhparkash1791@gmail.com>
2023-06-01 17:14:01 +0530

    modified:   BINARY_TREE/basic_functions.cpp 	modified:   BINARY_TREE/basic_functions.exe 	new file:   GENERIC_TREES/basic_functions.cpp

--- BINARY_TREE/basic_functions.cpp
+++ BINARY_TREE/basic_functions.cpp
@@ -239,27 +239,79 @@ int height_of_tree_recursive(struct bt *root){
 }
 int height_of_tree(struct bt *root){
     struct queue *q=create_queue();
-    int level,i;
+    int height=0;
     if(root){
         enqueue(q,root);
-        level=0;
-        i=1;      //no. of nodes at level 1
+        enqueue(q,NULL);
     }
     while(q->size>0){
-        for(int j=1;j<=i && q->size;j++){
-            bt *node=dequeue(q);
+        bt *node=dequeue(q);
+        if(node!=NULL){
             if(node->left)
                 enqueue(q,node->left);
             if(node->right)
                 enqueue(q,node->right);
         }
-        level++;
-        i=1;
-        for(int j=1;j<=level;j++){
-            i*=2;
+        else{
+            height++;
+            if(q->size==0){
+                break;
+            }
+            enqueue(q,NULL);
         }
     }
-    return (level-1);
+    return (height-1);
+}
+int min_depth_of_tree_recursive(struct bt *root){
+    if(root){
+        if(root->left && root->right){
+            int ln=min_depth_of_tree_recursive(root->left);
+            int rn=min_depth_of_tree_recursive(root->right);
+            return ((ln<rn)?(ln+1):(rn+1));
+        }
+        else if(root->left){
+            int ln=min_depth_of_tree_recursive(root->left);
+            return ln+1;
+        }
+        else if(root->right){
+            int rn=min_depth_of_tree_recursive(root->right);
+            return rn+1;
+        }
+        else{     //!(root->left) && !(root->right)
+            return 1;
+        }
+    }
+    else 
+        return 0;
+}
+int min_depth_of_tree(struct bt *root){
+    queue *q=create_queue();
+    int depth=0;
+    if(root){
+        enqueue(q,root);
+        enqueue(q,NULL);
+    }
+    while(q->size>0){
+        bt *node=dequeue(q);
+        if(node!=NULL){
+            if(!(node->left) && !(node->right)){
+                depth++;
+                break;
+            }
+            else{
+                if(node->left)
+                    enqueue(q,node->left);
+                if(node->right)
+                    enqueue(q,node->right);
+            }
+        }
+        else{      //node==NULL
+            enqueue(q,NULL);
+            depth++;
+        }  
+    }
+    return depth;
+    
 }
 int deepest_element(struct bt *root){
     struct queue *q=create_queue();
@@ -454,7 +506,7 @@ int path_with_sum(struct bt *root,int sum,int path[],int path_length){
     if(root){
         path[path_length]=root->data;
         path_length++;
-        if(!(root->left) && !(root->right)){
+        if(!(root->left) && !(root->right)){  //leaf node
             int s=0;
             for(int j=0;j<path_length;j++){
                 s+=path[j];
@@ -462,14 +514,247 @@ int path_with_sum(struct bt *root,int sum,int path[],int path_length){
             if(s==sum){
                 return path_length;
             }
+            else 
+                return -1;
         }
         else{
-            path_with_sum(root->left,sum,path,path_length);
-            path_with_sum(root->right,sum,path,path_length);
+            int l_path_sum=path_with_sum(root->left,sum,path,path_length);
+            if(l_path_sum!=-1)
+                return l_path_sum;
+            
+            int r_path_sum=path_with_sum(root->right,sum,path,path_length);
+            if(r_path_sum!=-1)
+                return r_path_sum;
+            else 
+                return -1;
         }
     }
     else
         return -1;
+}
+void mirror_bt(struct bt *root){
+    if(root){
+        mirror_bt(root->left);
+        mirror_bt(root->right);
+        bt *temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+    }
+    return;
+}
+bool check_mirror(struct bt *root_1, struct bt *root_2){
+    if(root_1 && root_2){
+        if(root_1->data==root_2->data){
+            bool c1=check_mirror(root_1->left,root_2->right);
+            bool c2=check_mirror(root_1->right,root_2->left);
+            if(c1 && c2)
+                return true;
+            else
+                return false;
+        }
+        else 
+            return false;
+    }
+    else if(!(root_1) && !(root_2))
+        return true;
+    else    
+        return false;
+}
+bt *inorder_preorder_2_binary_tree(int *inorder,int *preorder,int inorder_length){
+    struct bt *root=NULL;
+    struct bt **droot=&root;
+    root=new_node(preorder[0]);
+    //int i=0;
+    int e1=preorder[0];
+    int j=0;
+    while(inorder[j]!=e1)   
+        j++;
+    if(j>0){
+        root->left=inorder_preorder_2_binary_tree(inorder,preorder+1,j);
+        root->right=inorder_preorder_2_binary_tree(inorder+j+1,preorder+j+1,inorder_length-j-1);
+    }
+    else if(j==0){
+        root->left=NULL;
+        root->right=inorder_preorder_2_binary_tree(inorder+j+1,preorder+j+1,inorder_length-j-1);
+    }
+    else if(j==inorder_length-1){
+        root->left=inorder_preorder_2_binary_tree(inorder,preorder+1,j);
+        root->right=NULL;
+    }
+    return root;    
+}
+int ancestors(bt *root,int data,int anc[],int len){
+    if(root){
+        if(root->data==data){
+            anc[len]=data;
+            //len++;
+            return 1;
+        }
+        else{
+            anc[len]=root->data;
+            len++;
+            int l_len=ancestors(root->left,data,anc,len);
+            if(l_len==1){
+                return len+1;
+            }
+            else if(l_len>1){
+                return l_len;
+            }
+            int r_len=ancestors(root->right,data,anc,len);
+            if(r_len==1){
+                return len+1;
+            }
+            else if(r_len>1){
+                return r_len;
+            }
+
+            else if(l_len==-1 || r_len==-1){
+                return -1;
+            }
+        }
+    }
+    else{
+        return -1;
+    }
+}
+
+int sum(bt *root){
+    if(root){
+        return (root->data+sum(root->left)+sum(root->right));
+    }
+    else{
+        return 0;
+    }
+}
+int vertical_sum(bt *root,char position){ //position='c','l','r/R'=>centre,left,right
+    if(root){
+        if(position=='c'){
+            int left_node_sum=vertical_sum(root->left,'l');
+            int right_node_sum=vertical_sum(root->right,'r');
+            cout<<root->data+left_node_sum+right_node_sum<<" ";
+            vertical_sum(root->right,'R');
+            return 0;
+        }
+        else if(position=='l'){
+            if(!(root->left) && !(root->right)){
+                cout<<root->data<<" ";
+                return 0;
+            }
+            else if(!(root->left) && (root->right)){
+                cout<<root->data<<" ";
+                return sum(root->right);
+            }
+            else{        //(root->left)
+                int previous_right_node_sum=vertical_sum(root->left,'l');
+                cout<<root->data+previous_right_node_sum<<" ";
+                if(root->right){
+                    int total=sum(root->right);
+                    return total;
+                }
+            }
+        }
+        else if(position=='r'){
+            return sum(root->left);
+        }
+        else if(position=='R'){
+            if(!(root->right) && !(root->left)){
+                cout<<root->data<<endl;
+                return 0;
+            }
+            else if(!(root->right) && (root->left)){
+                cout<<root->data<<endl;
+                return 0;
+            }
+            else{        //(root->left)
+                int previous_left_node_sum=vertical_sum(root->right,'r');
+                cout<<root->data+previous_left_node_sum<<" ";
+                vertical_sum(root->right,'R');
+            }
+        }
+    }
+}
+
+bool check_isomorphism(bt *root_1,bt *root_2){
+    if(root_1 && root_2){
+        bool left=check_isomorphism(root_1->left,root_2->left);
+        bool right=check_isomorphism(root_1->right,root_2->right);
+        if(left && right){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else if(!(root_1) && !(root_2)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool check_isomorphism_with_data(bt *root_1,bt *root_2){
+    if(root_1 && root_2){
+        if(root_1->data!=root_2->data){
+            return false;
+        }
+        bool left=check_isomorphism_with_data(root_1->left,root_2->left);
+        bool right=check_isomorphism_with_data(root_1->right,root_2->right);
+        if(left && right){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else if(!(root_1) && !(root_2)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool check_quasi_isomorphism(bt *root_1,bt *root_2){
+    if(root_1 && root_2){
+        bool left=check_quasi_isomorphism(root_1->left,root_2->left);
+        bool right=check_quasi_isomorphism(root_1->right,root_2->right);
+        if(!left || !right){
+            left=check_quasi_isomorphism(root_1->left,root_2->right);
+            right=check_quasi_isomorphism(root_1->right,root_2->left);
+        }
+        if(left && right)
+            return true;
+        else 
+            return false;        
+    }
+    else if((!(root_1)&& root_2) || root_1 && !(root_2)){
+        return false;
+    }
+    else if(!(root_1) && !(root_2)){
+        return true;
+    }
+}
+bool check_quasi_isomorphism_with_data(bt *root_1,bt *root_2){
+    if(root_1 && root_2){
+        if(root_1->data !=root_2->data){
+            return false;
+        }
+        bool left=check_quasi_isomorphism_with_data(root_1->left,root_2->left);
+        bool right=check_quasi_isomorphism_with_data(root_1->right,root_2->right);
+        if(!left || !right){
+            left=check_quasi_isomorphism_with_data(root_1->left,root_2->right);
+            right=check_quasi_isomorphism_with_data(root_1->right,root_2->left);
+        }
+        if(left && right)
+            return true;
+        else 
+            return false;        
+    }
+    else if((!(root_1)&& root_2) || root_1 && !(root_2)){
+        return false;
+    }
+    else if(!(root_1) && !(root_2)){
+        return true;
+    }
 }
 int main(){
     struct bt *root_1=NULL;
@@ -483,18 +768,21 @@ int main(){
     insert(droot_1,7);
     insert(droot_1,8);
     //insert(droot_1,9);
+    cout<<"PRE-ORDER traversal: ";
     pre_order_traversal_recursive(root_1);
-    cout<<endl;
+    cout<<endl<<"IN-ORDER traversal: ";
     in_order_traversal_recursive(root_1);
-    cout<<endl;
+    cout<<endl<<"POST-ORDER traversal: ";
     post_order_traversal_recursive(root_1);
-    cout<<endl;
+    cout<<endl<<"LEVEL-ORDER traversal: ";
     level_order_traversal(root_1);
     cout<<endl;
     cout<<"MAX Element: "<<max_element(root_1)<<endl;
     cout<<"MAX Element using recursion: "<<max_element_using_recursion(root_1)<<endl;
     cout<<"HEIGHT/DEPTH of tree: "<<height_of_tree(root_1)<<endl;   
     cout<<"HEIGHT/DEPTH of tree using recursion: "<<height_of_tree_recursive(root_1)<<endl;
+    cout<<"MIN DEPTH of tree : "<<min_depth_of_tree(root_1)<<endl;
+    cout<<"MIN DEPTH of tree using recursion: "<<min_depth_of_tree_recursive(root_1)<<endl;
     cout<<"DEEPEST element: "<<deepest_element(root_1)<<endl;
     cout<<"No. of leaves: "<<no_of_leaves(root_1)<<endl;
     cout<<"No. of leaves using recursion: "<<no_of_leaves_recursive(root_1)<<endl;
@@ -503,13 +791,63 @@ int main(){
     cout<<"No. of half nodes: "<<no_of_half_nodes(root_1)<<endl;
     cout<<"No. of half nodes using recursion: "<<no_of_half_nodes_recursive(root_1)<<endl;   
     cout<<"Level with max. sum: "<<level_with_max_sum(root_1)<<endl;
+    cout<<"SUM of all elements: "<<sum(root_1)<<endl;
     int *path=new int[1];
+    cout<<"All possible paths from root node:"<<endl;
     print_root_2_leave_paths(root_1,path,0);
-    int path_length=path_with_sum(root_1,20,path,0);
-    for(int j=0;j<path_length;j++){
-        cout<<path[j]<<" ";
-    }
     cout<<endl;
+    int path_length=path_with_sum(root_1,24,path,0);
+    if(path_length!=-1){
+        cout<<"PATH with the given sum: ";
+        for(int j=0;j<path_length;j++){
+            cout<<path[j]<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        cout<<"NO PATH exists with the given sum"<<endl;
+    }
+
+    struct bt *root_2=NULL;
+    struct bt **droot_2=&root_2;
+    insert(droot_2,10);
+    insert(droot_2,2);
+    insert(droot_2,3);
+    insert(droot_2,4);
+    insert(droot_2,5);
+    insert(droot_2,6);
+    insert(droot_2,7);
+    insert(droot_2,8);
+    mirror_bt(root_2);
+    cout<<"LEVEL-ORDER traversal: ";
+    level_order_traversal(root_2);
+    cout<<endl;
+    cout<<"BT_1 and BT_2 are mirrors or not: "<<((check_mirror(root_1,root_2))?("YES"):("NO"))<<endl;
+    int inorder[]={1,2,3,4,5,6};
+    int preorder[]={4,2,1,3,6,5};
+    //struct bt *root_3=inorder_preorder_2_binary_tree(inorder,preorder,6);
+    int *anc=new int[1];
+    int len=ancestors(root_2,8,anc,0);
+    if(len!=-1){
+        cout<<"PATH with the given number: ";
+        for(int j=0;j<len;j++){
+            cout<<anc[j]<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        cout<<"NO PATH exists with the given number"<<endl;
+    }
+    cout<<"VERTICAL SUM OF BINARY TREE: ";
+    vertical_sum(root_1,'c');
+    cout<<"BT_1 and BT_2 are isomorphic or not: "<<((check_isomorphism(root_1,root_2))?("YES"):("NO"))<<endl;
+    cout<<"BT_1 and BT_1 are isomorphic or not: "<<((check_isomorphism(root_1,root_1))?("YES"):("NO"))<<endl;
+    cout<<"BT_1 and BT_2 are isomorphic(with data) or not: "<<((check_isomorphism_with_data(root_1,root_2))?("YES"):("NO"))<<endl;
+    
+    cout<<"BT_1 and BT_2 are quasi-isomorphic or not: "<<((check_quasi_isomorphism(root_1,root_2))?("YES"):("NO"))<<endl;
+    cout<<"BT_1 and BT_1 are quasi-isomorphic or not: "<<((check_quasi_isomorphism(root_1,root_1))?("YES"):("NO"))<<endl;    
+    cout<<"BT_1 and BT_2 are quasi-isomorphic(with data) or not: "<<((check_quasi_isomorphism_with_data(root_1,root_2))?("YES"):("NO"))<<endl;
+
     //del_tree(droot_1);
     //pre_order_traversal(root_1);
 }
---
+++ GENERIC_TREES/basic_functions.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+struct tree{
+    int data;
+    struct tree *first_child;
+    struct tree *first_sibling;
+};
+int main(){
+
+}
